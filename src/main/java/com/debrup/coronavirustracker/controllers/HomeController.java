d3d66999f0acf7e2ece3e28a04065889c9a54f0d
Debrup Mondal <debrup.official.365@gmail.com>
2021-02-19 00:14:09 +0530

    final commit

---
+++ src/main/java/com/debrup/coronavirustracker/controllers/HomeController.java
@@ -0,0 +1,30 @@
+package com.debrup.coronavirustracker.controllers;
+
+import com.debrup.coronavirustracker.models.LocationStats;
+import com.debrup.coronavirustracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
+        return "home";
+    }
+
+
+}
---
+++ src/test/java/com/debrup/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debrup.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
